823bfc74a9c5bfdc39f511db01b902e0c3f27506
juanmafc <juanmafc25@gmail.com>
2016-06-28 08:09:58 -0300

    muevo de lugar la prueba

--- requests-2.10.0/prueba.py
+++ requests-2.10.0/prueba.py
@@ -2,5 +2,5 @@
 import requests.packages.urllib3
 
 if __name__ == '__main__':
-    reply = requests.get("http://t2shared.herokuapp.com/interests/")
+    reply = requests.requests.packages.urllib3.get("http://t2shared.herokuapp.com/interests/")
     print reply.status_code
