cdd301071ac72959abe297d11a315fad8ea319c7
juanmafc <juanmafc25@gmail.com>
2016-06-28 09:56:46 -0300

    Ahora con pythonpath pero tambien con un archivo utilities para ver si lo busca

--- prueba.py
+++ prueba.py
@@ -1,6 +1,8 @@
 import requests
 #import requests.packages.urllib3
+import utilities
 
 if __name__ == '__main__':
+    utilities.printearInicio()
     reply = requests.get("http://t2shared.herokuapp.com/interests/")
     print reply.status_code
